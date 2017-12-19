3d542e2a024c891f480697aa3482de4a75681c24
Alex Miller <amiller@launchcg.com>
2017-12-19 15:27:18 -0800

    cleaning files

--- wp-config.php
+++ wp-config.php
@@ -37,8 +37,6 @@ define('DB_CHARSET', 'utf8');
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
 
-echo 'host:'.DB_HOST;
-
 /**
  * For developers: WordPress debugging mode.
  *
